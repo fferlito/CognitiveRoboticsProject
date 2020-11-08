b80ac282d9062925122b8c5059e972f23fb921ad
Ferlix <ferli_96@live.it>
2020-11-08 18:36:43 +0100

    fixed a number

--- train.py
+++ train.py
@@ -46,9 +46,9 @@ def train_and_test(args):
             print('Quitting the code..')
             
         if args.strategy == 0:
-            samples_used.append(mf.fit_using_al_strategy_thres_intermediate_update(data[training_idxs], labels[training_idxs],np.array(range(51)), inital_dataset_size=3, threshold=args.threshold))
+            samples_used.append(mf.fit_using_al_strategy_thres_intermediate_update(data[training_idxs], labels[training_idxs],np.array(range(51)), inital_dataset_size=1000, threshold=args.threshold))
         elif args.strategy == 1:
-            samples_used.append(mf.our_al_strategy(data[training_idxs], labels[training_idxs], np.array(range(51)), 300, args.threshold))
+            samples_used.append(mf.our_al_strategy(data[training_idxs], labels[training_idxs], np.array(range(51)), 1000, args.threshold))
         elif args.strategy == 2:
             samples_used.append(mf.second_al_strategy(data[training_idxs], labels[training_idxs], np.array(range(51)), 1000))
         else:
